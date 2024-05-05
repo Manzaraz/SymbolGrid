6fc9c08a4ad9ce680a4f7dbd22b0eec06a95d37f
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-04 23:53:38 -0300

    Initial Commit

---
+++ SimbolGrid/SimbolGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimbolGridApp.swift
+//  SimbolGrid
+//
+//  Created by Christian Manzaraz on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
